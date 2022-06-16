182446b9f846a5efc54c33b7369ccf32ec40b912
CHENZhoujing <zhoujingchen1@gmail.com>
2022-06-16 14:46:27 +0200

    优化读取excel的bug 删去_CompressPdf后缀

--- CreateFloder.py
+++ CreateFloder.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import time
 from openpyxl import load_workbook, Workbook
 from openpyxl.worksheet.worksheet import Worksheet
@@ -38,6 +39,9 @@ class CreateFolder:
                 self.dict_agence[key] = 1
             else:
                 self.dict_agence[key] = value + 1
+            #出现空白表格跳出
+            if self.cell[i][1].value is None:
+                break
 
     def mkdir(self, path):
         folder = os.path.exists(path)
@@ -45,16 +49,16 @@ class CreateFolder:
             os.makedirs(path)
 
     def create_folder(self):
-        #1
+        #1 第一层
         folder_name = self.date + " Paris 02 " + str(self.my_sheet.max_row - 1) + "家" #去除第一行
         self.mkdir(folder_name)
         self.path = self.path + os.sep + folder_name
-        #2
+        #2 第二层
         for k in self.dict_agence.keys():
             v = self.dict_agence.get(k)
             path_inner = self.path + os.sep + str(self.date) + " " + str(k) + " " + str(v) + "家"
             self.mkdir(path_inner)
-            #3
+            #3 第三层
             self.create_folder_sup(k, v, path_inner)
 
     def create_folder_sup(self, agence_name, agence_nbr, path_inner):
@@ -62,9 +66,19 @@ class CreateFolder:
         while 0 < agence_nbr and cpt < self.my_sheet.max_row:
             if self.cell[cpt][1].value == agence_name:
                 folder_name = str(self.cell[cpt][0].value) + " " + self.cell[cpt][3].value.upper()
-                self.mkdir(path_inner + os.sep + folder_name)
+                path_inner_inner = path_inner + os.sep + folder_name
+                self.mkdir(path_inner_inner)
+                self.move_docs(path_inner_inner, str(self.cell[cpt][0].value)) #prefix
                 agence_nbr -= 1
             cpt += 1
+
+    def move_docs(self, path_inner_inner: str, prefix: str):
+        for file in self.file_list:
+            if "_CompressPdf" in file:
+                os.rename(self.path_pdf + os.sep + file, self.path_pdf + os.sep + file.replace("_CompressPdf", ""))
+            if ".pdf" in file and prefix in file:
+                shutil.move(self.path_pdf + os.sep + file, path_inner_inner + os.sep + file)
+
 
     def create_xlsx_doc(self):
         doc = Workbook()
@@ -93,9 +107,6 @@ class CreateFolder:
             cpt += 1
         date = time.strftime("%d.%m.%Y", time.localtime())
         doc.save(self.path + os.sep + date + " Créations M0 - " + str(self.my_sheet.max_row - 1) + "sociétés.xlsx")
-
-       #def:
-
 
 file_name = os.getcwd()
 cf = CreateFolder(file_name)
