2b3f9add2c85e7551b4ac77aee85671303bf1fb8
CHENZhoujing <zhoujingchen1@gmail.com>
2022-06-22 10:25:47 +0200

    把SEPARER转换为Separate class化

--- CreateFloder.py
+++ CreateFloder.py
@@ -4,6 +4,7 @@ import time
 from openpyxl import load_workbook, Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 
+
 class CreateFolder:
     file_list: list[str]
     path_pdf: str
@@ -15,11 +16,11 @@ class CreateFolder:
     cell: tuple
 
     def __init__(self, path):
-        #初始化日期
+        # 初始化日期
         self.date = time.strftime("%Y.%m.%d", time.localtime())
-        #pdf文件
+        # pdf文件
         self.path_pdf = path
-        #读取文件
+        # 读取文件
         self.path = path
         self.file_list = os.listdir(self.path)
         file_name: str = ""
@@ -30,7 +31,7 @@ class CreateFolder:
         self.wb = load_workbook(file_name, read_only=True)
         self.my_sheet = self.wb.active
         self.cell = tuple(self.my_sheet)
-        #统计AGENCE数量
+        # 统计AGENCE数量
         self.dict_agence = {}
         for i in range(1, self.my_sheet.max_row):
             key = str(self.cell[i][1].value)
@@ -39,7 +40,7 @@ class CreateFolder:
                 self.dict_agence[key] = 1
             else:
                 self.dict_agence[key] = value + 1
-            #出现空白表格跳出
+            # 出现空白表格跳出
             if self.cell[i][1].value is None:
                 break
 
@@ -55,16 +56,16 @@ class CreateFolder:
         self.file_list = os.listdir(self.path)
 
     def create_folder(self):
-        #1 第一层
-        folder_name = self.date + " Paris 02 " + str(self.my_sheet.max_row - 1) + "家" #去除第一行
+        # 1 第一层
+        folder_name = self.date + " Paris 02 " + str(self.my_sheet.max_row - 1) + "家"  # 去除第一行
         self.mkdir(folder_name)
         self.path = self.path + os.sep + folder_name
-        #2 第二层
+        # 2 第二层
         for k in self.dict_agence.keys():
             v = self.dict_agence.get(k)
             path_inner = self.path + os.sep + str(self.date) + " " + str(k) + " " + str(v) + "家"
             self.mkdir(path_inner)
-            #3 第三层
+            # 3 第三层
             self.create_folder_sup(k, v, path_inner)
 
     def create_folder_sup(self, agence_name, agence_nbr, path_inner):
@@ -74,7 +75,7 @@ class CreateFolder:
                 folder_name = str(self.cell[cpt][0].value) + " " + self.cell[cpt][3].value.upper()
                 path_inner_inner = path_inner + os.sep + folder_name
                 self.mkdir(path_inner_inner)
-                self.move_docs(path_inner_inner, str(self.cell[cpt][0].value)) #prefix
+                self.move_docs(path_inner_inner, str(self.cell[cpt][0].value))  # prefix
                 agence_nbr -= 1
             cpt += 1
 
@@ -83,11 +84,10 @@ class CreateFolder:
             if ".pdf" in file and prefix in file:
                 shutil.move(self.path_pdf + os.sep + file, path_inner_inner + os.sep + file)
 
-
     def create_xlsx_doc(self):
         doc = Workbook()
         docs = doc.active
-        row = ["N°", "Date d’envoi", "Nom", "Code Postal", "Ville", "Pays","Date de début d’acitivité",
+        row = ["N°", "Date d’envoi", "Nom", "Code Postal", "Ville", "Pays", "Date de début d’acitivité",
                "Activités", "Nom du correspondant", "Complément d’adresse", "Adresse", "Date de signature", "TVAs"]
         docs.append(row)
         row[1] = time.strftime("%d/%m/%Y", time.localtime())
@@ -113,6 +113,7 @@ class CreateFolder:
                 break
         date = time.strftime("%d.%m.%Y", time.localtime())
         doc.save(self.path + os.sep + date + " Créations M0 - " + str(self.my_sheet.max_row - 1) + "sociétés.xlsx")
+
 
 file_name = os.getcwd()
 cf = CreateFolder(file_name)
